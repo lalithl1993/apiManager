92305a997e59eae5b7956f032ccd7636af6a4a78
lalithl1993 <lalithl1993@gmail.com>
2018-06-21 18:08:54 +0530

    add class impliment getBookings

--- apiManager/onnbikes/class.onn.php
+++ apiManager/onnbikes/class.onn.php
@@ -1,8 +1,74 @@
 <?php
 
-include"../class.CurlHelper.php";
+require "api.onn.php";
+class class_onn
+{
 
-    $action = "POST";
+$authToken="5b1f666d0ed5422cfbabfb13";
+
+$api = new api_onn();
+
+function getBookings($payLoad)
+{
+
+
+$parameters = array (
+  'authToken' => $authToken,
+  'fromDate' => $payLoad['fromDate'],
+  'toDate' => $payLoad['toDate'],
+  'cityLatitude' => $payLoad['cityLatitude'],
+  'cityLongitude' => $payLoad['cityLongitude'],
+  'cityName' => $payLoad['cityName'],
+);
+$parametersJSON=json_encode($parameters);
+
+//$result = CurlHelper::perform_http_request($httpMethod, $url, $parametersJSON);
+
+$result = $api->getBookings($parametersJSON) //makeing function call from api.onn.php
+
+if($result[errno]!=0||$result[error]!="")
+{
+// log error occured $result[error]
+return false;
+}
+else if(isset($result[response]))
+{
+$resultArray=json_decode($result[response]);
+if($resultArray[errorCode]!=0||$resultArray[errorMessage]!="Success")
+{
+// log $resultArray[errorMessage]
+return false;
+}
+}
+else{
+return($resultArray);// incorrect
+//todo: do some logical operation for data manupulation and return data which is compatable with our application
+}
+}
+
+
+
+
+
+}//end of class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*   $action = "POST";
     $url = "https://devservice.onnbikes.com/OnnBikesExternal/v1/rest/booking/getAvailableBikes";
     
 	
@@ -25,6 +91,6 @@ $result = CurlHelper::perform_http_request($action, $url, $parametersJSON); // m
 
    echo print_r($result);
 	
-	
+	*/
 //check for errors----> $result['error'],$result['errno'],$result['result'](error message from json response)
 ?>
