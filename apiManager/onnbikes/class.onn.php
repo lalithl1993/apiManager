93ad0bf3e31c5e622468f610c552c7861f990d54
lalithl1993 <lalithl1993@gmail.com>
2018-06-22 08:53:10 +0530

    added filterResponse

--- apiManager/onnbikes/class.onn.php
+++ apiManager/onnbikes/class.onn.php
@@ -6,59 +6,73 @@ require "api.onn.php";
 class class_onn
 {
 
-private $authToken;
+    private $authToken;
 
-private $api;
+    private $api;
 
-public function __construct()
-{
-    $this->authToken = "5b1f666d0ed5422cfbabfb13";
-    $this->api = new api_onn;
-}
+    public function __construct()
+    {
+        $this->authToken = "5b1f666d0ed5422cfbabfb13";
+        $this->api = new api_onn;
+    }
 
-public function getAvailableBikes($payLoad)
-{
+    private function isJson($string)
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    private function filterResponse($result)
+    {
+        if(trim($result['errno']) != 0 && trim($result['error']) != "")// checking for http errors
+        {
+            // todo: log error occured $result[error]
+            return false;
+        }
+
+        if(trim($result['response']) == "")
+        {
+            // todo: log empty response error
+            return false;
+        }
+        if(class_onn::isJson($result['response']))
+        {
+            $result['response']=json_decode($result['response'],true);//convering json to associative array
+            if(($result['response']['errorCode'] != 0) || ($result['response']['errorMessage'] != "Success"))// checking for errors from API
+            {
+                // todo: log $resultArray[errorMessage]
+                return false;
+            }
+        }
 
 
-$parameters = array (
-  'authToken' => $this->authToken,
-  'fromDate' => $payLoad['fromDate'],
-  'toDate' => $payLoad['toDate'],
-  'cityLatitude' => $payLoad['cityLatitude'],
-  'cityLongitude' => $payLoad['cityLongitude'],
-  'cityName' => $payLoad['cityName'],
-);
+        return $result['response'];
+    }
 
-$parametersJSON=json_encode($parameters);
+    public function getAvailableBikes($payLoad)
+    {
 
 
-$result = $this->api->getAvailableBikes($parametersJSON); //makeing function call from api.onn.php
+        $parameters = array (
+          'authToken' => $this->authToken,
+          'fromDate' => $payLoad['fromDate'],
+          'toDate' => $payLoad['toDate'],
+          'cityLatitude' => $payLoad['cityLatitude'],
+          'cityLongitude' => $payLoad['cityLongitude'],
+          'cityName' => $payLoad['cityName'],
+        );
 
-$resultArray="";
+        $parametersJSON=json_encode($parameters);
 
-if($result['errno']!=0&&$result['error']!="")// checking for http errors
-{
-// todo: log error occured $result[error]
-return false;
-}
-else if($result['response']!="")
-{
-$resultArray=json_decode($result['response'],true);
 
-if($resultArray['errorCode']!=0||$resultArray['errorMessage']!="Success")// checking for errors from API
-{
-// todo: log $resultArray[errorMessage]
-return false;
-}
-else{
- 
-return($resultArray);// incorrect
-//todo: do some logical operation for data manupulation and return data which is compatable with our application
-}
-}
-return false;
-   
-}
+        $result = $this->api->getAvailableBikes($parametersJSON); //makeing function call from api.onn.php
+
+        if($response=class_onn::filterResponse($result))
+        {
+            return $response;
+        }
+        return false;
+    }
 
 
 
